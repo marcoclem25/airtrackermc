23272068a34304a1841be026a58c61bf39ac4978
Marco Clemente <marcoclemente@Air-di-Marco-3.Home>
2026-03-24 13:48:06 +0100

    fix title

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -17,7 +17,7 @@ export default function Welcome({
 
     return (
         <>
-            <Head title="Welcome">
+            <Head title="AirTrackerMC - Monitora i tuoi voli">
                 <link rel="preconnect" href="https://fonts.bunny.net" />
                 <link
                     href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600"
@@ -54,7 +54,7 @@ export default function Welcome({
                         )}
                     </nav>
                 </header>
-                
+
                 <div className="flex w-full items-center justify-center opacity-100 transition-opacity duration-750 lg:grow starting:opacity-0">
                     <main className="flex w-full max-w-[335px] flex-col-reverse lg:max-w-4xl lg:flex-row">
                         <div className="flex-1 rounded-br-lg rounded-bl-lg bg-white p-6 pb-12 text-[13px] leading-[20px] shadow-[inset_0px_0px_0px_1px_rgba(26,26,0,0.16)] lg:rounded-tl-lg lg:rounded-br-none lg:p-20 dark:bg-[#161615] dark:text-[#EDEDEC] dark:shadow-[inset_0px_0px_0px_1px_#fffaed2d]">
